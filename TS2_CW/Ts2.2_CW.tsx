c6451413cfe36c2a014709a85fd6cfdb6554fb98
Pradeep Maurya <aradshub93@gmail.com>
2024-03-24 13:43:30 +0530

    added generics for map,filter,reduce

--- TS2_CW/Ts2.2_CW.tsx
+++ TS2_CW/Ts2.2_CW.tsx
@@ -27,14 +27,61 @@ const checkQuizAnswer: quizChecker = (quizQuestion, userAnswer) => {
   return userAnswer === quizQuestion.correctOption;
 };
 
-// function quizChecker(question: QuizQuestion, userAnswer: number): boolean {
-//   if (question.correctOption === userAnswer) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-
 // ex 03: type for filtering array
 
 type filterFunction<T> = (array: T[], predicate: (value: T) => boolean) => T[];
+
+const isEven = (num: number) => num % 2 === 0;
+const numbers = [1, 2, 3, 4, 5];
+
+const filterArray: filterFunction<number> = (list, predicate) => {
+  return list.filter(predicate);
+};
+
+console.log(filterArray(numbers, isEven));
+
+// ex 04 type for map array
+
+type mapFunction<T, R> = (list: T[], mapper: (value: T) => R) => R[];
+
+const doubleToString = (num: number) => (num * 2).toString();
+const numbersList = [1, 2, 3, 4, 5];
+
+const mapArray: mapFunction<number, string> = (list, mapper) => {
+  return list.map(mapper);
+};
+
+console.log(mapArray(numbersList, doubleToString));
+
+// ex 05 type for reducing array
+
+type reduceFunction<T, R> = (
+  list: T[],
+  accumulator: (acc: R, value: T) => R,
+  initialValue: R
+) => R;
+
+const numberList = [1, 2, 3, 4, 5];
+const sumAccumulator = (acc: number, num: number) => acc + num;
+
+const reduceArray: reduceFunction<number, number> = (
+  list,
+  acc,
+  initialValue
+) => {
+  return list.reduce(acc, initialValue);
+};
+
+console.log(reduceArray(numberList, sumAccumulator, 0));
+
+const reduceStringArray: reduceFunction<string, string> = (
+  list,
+  acc,
+  initialValue
+) => {
+  return list.reduce(acc, initialValue);
+};
+
+const stringSumAcc = (acc: string, char: string) => acc + char;
+
+console.log(reduceStringArray(["a", "b", "c", "d", "e"], stringSumAcc, ""));
